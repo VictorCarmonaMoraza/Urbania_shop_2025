22a8f1ed5e86e58d65a10ca72ff88b270d8d40d6
Victor Carmona <victorcarmona9035.com>
2025-08-11 18:26:28 +0200

    Paginas y rutas

---
+++ src/app/store-front/front-navbar/front-navbar.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'front-navbar',
+  imports: [],
+  templateUrl: './front-navbar.html',
+  styleUrl: './front-navbar.css'
+})
+export class FrontNavbar {
+
+}
---
+++ src/app/store-front/layouts/store-front-layout/store-front-layout.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FrontNavbar } from "../../front-navbar/front-navbar";
+
+
+@Component({
+  selector: 'app-store-front-layout',
+  imports: [RouterOutlet, FrontNavbar],
+  templateUrl: './store-front-layout.html',
+  styleUrl: './store-front-layout.css'
+})
+export class StoreFrontLayout {
+
+}
