609a84f0c5a26e9f0222af5799d8f75815c429b6
Victor Carmona <victorcarmona9035.com>
2025-08-15 12:22:35 +0200

    Tarjetas de productos y FadeIn animation

---
+++ src/app/store-front/components/product-card/product-card.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+
+@Component({
+  selector: 'product-card',
+  imports: [RouterLink],
+  templateUrl: './product-card.html',
+  styleUrl: './product-card.css'
+})
+export class ProductCard {
+
+}
