18a92d9bc44b205e8ee2c338332c94c33d0d80c7
Victor Carmona <victorcarmona9035.com>
2025-08-15 13:35:06 +0200

    Typescript - Path Alias

--- src/app/store-front/pages/home-page/home-page.ts
+++ src/app/store-front/pages/home-page/home-page.ts
@@ -1,5 +1,6 @@
+import { ProductCard } from '@/products/components/product-card/product-card';
 import { Component } from '@angular/core';
-import { ProductCard } from "../../components/product-card/product-card";
+//import { ProductCard } from "../../components/product-card/product-card";
 
 @Component({
   selector: 'app-home-page',
